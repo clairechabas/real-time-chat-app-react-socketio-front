06ad7f9351dfcf29443805938466330e4c240298
Claire C <claire.chabas@gmail.com>
2022-11-29 21:08:00 +0100

    feat(join-form): Handling username and room change and redirecting user to chosen room

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 import Home from './pages/Home';
 
+const socket = io.connect('http://localhost:4000');
+
 function App() {
+  const [userName, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
     <Router>
       <div class="app">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                username={userName}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+  const navigate = useNavigate();
+
+  const joinRoom = () => {
+    if (username !== '' && room !== '') {
+      socket.emit('join_room', { username, room });
+    }
+
+    // Redirecting user to his room
+    navigate('/room', { replace: true });
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.formContainer}>
+      <form className={styles.formContainer} onSubmit={joinRoom}>
         <h1>{`👩‍💻 DevTalks 🧑‍💻`}</h1>
         <input
           className={styles.input}
           placeholder="🙋‍♀️ Who should we announce?"
+          onChange={(e) => setUsername(e.target.value)}
         />
 
-        <select className={styles.input}>
+        <select
+          className={styles.input}
+          onChange={(e) => setRoom(e.target.value)}
+        >
           <option>-- 🛋 Get a Room --</option>
           <option value="javascript">JavaScript</option>
           <option value="node">Node</option>
@@ -18,7 +34,9 @@ const Home = () => {
           <option value="vue">Vue</option>
         </select>
 
-        <button className="btn">Join Room</button>
+        <button type="submit" className="btn">
+          Join Room
+        </button>
       </form>
     </div>
   );
